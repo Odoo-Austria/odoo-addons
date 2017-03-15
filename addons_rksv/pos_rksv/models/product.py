6ce75d5d901072de1a567201b94239811f8a980e
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-03-15 14:56:58 +0100

    [IMP] Done? Hopefully :-) this needs to be improved from W2W ;-)

--- addons_rksv/pos_rksv/models/product.py
+++ addons_rksv/pos_rksv/models/product.py
@@ -10,7 +10,7 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.multi
-    @api.depends('taxes_id')
+    @api.depends('taxes_id', 'taxes_id.rksv_tax', 'taxes_id.rksv_tax_category')
     def _compute_rksv_tax_mapping_correct(self):
         for template in self:
             rksv_tax = False
