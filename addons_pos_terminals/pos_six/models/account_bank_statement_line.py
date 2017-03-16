529525497430caa0032705f8c6d13e8fd54162fa
Wolfgang Pichler <wpichler@callino.at>
2017-03-16 21:03:30 +0100

    Added POS Six Payment Terminal Support - Needs quality checking

---
+++ addons_pos_terminals/pos_six/models/account_bank_statement_line.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class ABStatementLine(models.Model):
+    _inherit = 'account.bank.statement.line'
+
+    six_ref_number = fields.Char('Six Ref Number')
+    six_receipt = fields.Char('Six Beleg')
+    six_receipt_html = fields.Char('Six Beleg')
+    six_receipt_merchant = fields.Char('Six Händler Beleg')
+    six_receipt_merchant_html = fields.Char('Six Händler Beleg')
