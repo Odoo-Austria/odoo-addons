e19aa2ec1fa3696e9c6742ed144cce2e007ec4f0
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-03-23 10:14:19 +0100

    [FIX] Wiederinbetriebnahme

--- addons_rksv/pos_rksv/static/src/js/rksv.js
+++ addons_rksv/pos_rksv/static/src/js/rksv.js
@@ -560,7 +560,7 @@ function openerp_rksv_rksv(instance) {
         rk_ausfalls_modus: function() {
             var self = this;
             var op_popup = this.pos.gui.popup_instances.rksv_popup_widget;
-            op_popup.show({}, 'Ausfallmodus der Signatureinheit aktivieren', 'Ausfallmodus');
+            op_popup.show({}, 'Signatureinheit Ausfallmodus aktivieren', 'Ausfallmodus');
             // First - do disable old event handlers
             op_popup.$('.execute_button').off();
             // Then install new click handler
@@ -957,6 +957,7 @@ function openerp_rksv_rksv(instance) {
                                     'bmf_message': 'Wiederinbetriebnahme erfolgreich gemeldet',
                                     'bmf_last_status': 'IN_BETRIEB'
                                 });
+                                self.pos.set('cashbox_mode', 'active');
                             }
                         },
                         function failed() {
