74fc03c2be6a98bc9aeb2cec436c96527a46e899
Wolfgang Pichler <wpichler@callino.at>
2017-03-24 12:20:45 +0100

    Added possibility to specify the starting receipt number when registering the cashbox

--- addons_rksv/pos_rksv/static/src/js/screens.js
+++ addons_rksv/pos_rksv/static/src/js/screens.js
@@ -472,9 +472,13 @@ function openerp_rksv_screens(instance, module) {
                     self.$('.rksv-status-indicator .indicator').css('background', 'red');
                     self.$('.rksv-status-indicator .indicator-message').html("Kasse ist deaktviert !");
                 }
+                /*
+                It should always be possible to use an other signature provider
+
                 if (self.pos.get('cashbox_mode') == 'active'){
                     self.$el.find('.sprovider-btn').hide()
                 }
+                */
                 if (self.pos.get('cashbox_mode') == 'signature_failed'){
                     self.$el.find('.sprovider-btn').show()
                 }
