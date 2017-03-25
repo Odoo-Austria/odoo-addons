b141add8c667d702ca45ff66dbd67ae3e412f36b
Wolfgang Pichler <wpichler@callino.at>
2017-03-25 10:19:15 +0100

    [ADD] Added rksv restaurant extension

---
+++ addons_rksv/pos_rksv_restaurant/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Registrierkasse Österreich Restaurant',
+    'version': '10.0.0.1',
+    'category': 'Point of Sale',
+    'sequence': 6,
+    'summary': 'Registrierkassenpflicht Modul für Österreich, Restaurant Erweiterung',
+    'website': 'https://www.callino.at/page/rksv',
+    'description': """
+Registrierkasse Österreich
+==================================
+
+Erweiterung zum Basis RKSV Modul für die Restaurant Erweiterung
+""",
+    'author': 'Wolfgang Pichler (Callino), Wolfgang Taferner (WT-IO-IT GmbH)',
+    'depends': ['pos_rksv', 'pos_restaurant'],
+    'test': [
+    ],
+    'data': [
+        'views/pos_config.xml',
+        'views/templates.xml',
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    "external_dependencies": {
+        "python": [],
+        "bin": []
+    },
+}
---
+++ addons_rksv/pos_rksv_restaurant/models/pos_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class pos_config(models.Model):
+    _name = 'pos.config'
+    _inherit = 'pos.config'
+    default_table_id = fields.Many2one(
+        comodel_name='restaurant.table',
+        string='Standard Tisch',
+        required=True
+    )
