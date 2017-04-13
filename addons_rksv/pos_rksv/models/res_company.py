450f26553d0cfa7188b534d06701e1f04182e2ba
Wolfgang Taferner <wtaferner@users.noreply.github.com>
2017-04-13 11:01:13 +0200

    [FIX] Minor fixes and structure

--- addons_rksv/pos_rksv/models/res_company.py
+++ addons_rksv/pos_rksv/models/res_company.py
@@ -11,8 +11,10 @@ class Company(models.Model):
     _inherit = 'res.company'
 
     bmf_tid = fields.Char('BMF TID', size=32, copy=False)
+
     bmf_benid = fields.Char('BMF Benutzer ID', size=32, copy=False)
     bmf_pin = fields.Char('BMF PIN', size=32, copy=False)
-    bmf_tax_number = fields.Char('RKSV Tax Identifier', size=10)
-    bmf_vat_number = fields.Char('RKSV VAT Identifier', size=10, readonly=True, related='vat')
     bmf_hersteller_atu = fields.Char('BMF RK Hersteller ATU', size=32, copy=False)
+
+    bmf_tax_number = fields.Char('RKSV Tax Identifier', size=10)
+    bmf_vat_number = fields.Char('RKSV VAT Identifier', readonly=True, related='vat')
